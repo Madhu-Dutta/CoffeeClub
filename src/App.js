b85ee5ed3717e3e560f9743f82a59935f59729f6
Madhu Dutta <mdutta787@gmail.com>
2019-11-26 10:53:04 +1100

    trying to fix netlify deployment error-try2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './Components/Home';
 import Landing from './Components/Landing';
 import Login from './Components/Login';
 import Register from './Components/Register';
-import PostRecord from './Components/PostRecord';
+import PostRecord from './Components/postRecord';
 import EditRecord from './Components/EditRecord';
 
 function App() {
@@ -14,12 +14,13 @@ function App() {
         <React.Fragment>
             <Switch>
               <Route exact path='/' component={Landing} />
+              {/* Edit & Post Records path */}
+              <Route exact path="/edit/:id" component={EditRecord} />
+              <Route exact path="/postRecord" component={PostRecord} />
               <Route path='/Login' component={Login} />
               <Route path='/Register' component={Register} />
               <Route path='/Home' component={Home} />
-              {/* Edit & Post Records path */}
-                <Route exact path="/edit/:id" component={EditRecord} />
-                <Route path="/PostRecord" component={PostRecord} />
+              
             </Switch>
         </React.Fragment>
     </BrowserRouter>
